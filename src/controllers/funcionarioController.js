d71e9f1765816349e12124f36c68defbfa750ee0
Matheus-Rodrigues-Caraca <theuscaraca@gmail.com>
2020-06-17 17:46:07 -0300

    Pesquisa por nome(tag)

--- src/controllers/funcionarioController.js
+++ src/controllers/funcionarioController.js
@@ -17,5 +17,14 @@ module.exports = {
         const nome = req.params.nome;
         const funcionarios = await Funcionario.findOne({nome}, function (err, item){ });
         return res.json(funcionarios);
+    },
+    async pesquisaTag (req, res){
+        const nome = req.params.nome;
+        
+        const funcionarios = await Funcionario.find({"tags": {$in: [nome]}}, function (err, item){ });
+        console.log(nome);
+        console.log(funcionarios);
+        return res.json(funcionarios);
     }
+
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -11,6 +11,7 @@ routes.get('/funcionarios', funcionarioController.index);
 
 routes.get('/funcionarios/:nome', funcionarioController.details); 
 
+routes.get('/funcionariosN/:nome', funcionarioController.pesquisaTag);
 //rotas vereadores e funcionarios da camara
 routes.post('/fcamara', vereadoresController.insert);
 
